a072800157a9d874ae5de2934dbbf8e4607c9b58
Jose Alejandro Pereiro Gabian <jpereirogabian@danielcastelao.org>
2022-02-16 14:40:44 +0100

    Initial commit

---
+++ src/com/cod/Pizzas.java
@@ -0,0 +1,16 @@
+package com.cod;
+
+public class Pizzas {
+    private String masa;
+    private boolean relleno;
+    private int tipo;
+    private boolean salsa;
+    private boolean cebolla;
+    private String tipoSalsa;
+    private boolean sinGluten;
+    private boolean extraQueso;
+    private boolean piña;
+    private boolean champiñones;
+    private boolean jamon;
+
+}
