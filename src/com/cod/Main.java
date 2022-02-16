0aede55f1848ef5411ba1444015eb925d305b038
Alejandro Pereiro G <jpereirogabian@danielcastelao.org>
2022-02-16 21:24:49 +0100

    Pizzas cocinadas

--- src/com/cod/Main.java
+++ src/com/cod/Main.java
@@ -3,6 +3,11 @@ package com.cod;
 public class Main {
 
     public static void main(String[] args) {
-
+        Pizzas margarita=new Pizzas("normal", false, 1, true, false, "solo tomate", false, false, false, false, false);
+        System.out.println(margarita.toString());
+        Pizzas prosciuttofunghi=new Pizzas("pan", false, 2, true, false, "solo tomate", true, true, false, true, true);
+        System.out.println(prosciuttofunghi.toString());
+        Pizzas barbacoa=new Pizzas("finisima", true, 1, true, true, "barbacoa", false, true, false, false, true);
+        System.out.println(barbacoa.toString());
     }
 }
--- src/com/cod/Pizzas.java
+++ src/com/cod/Pizzas.java
@@ -13,4 +13,125 @@ public class Pizzas {
     private boolean champiñones;
     private boolean jamon;
 
+    public Pizzas() {
+    }
+
+    public Pizzas(String masa, boolean relleno, int tipo, boolean salsa, boolean cebolla, String tipoSalsa, boolean sinGluten, boolean extraQueso, boolean piña, boolean champiñones, boolean jamon) {
+        this.masa = masa;
+        this.relleno = relleno;
+        this.tipo = tipo;
+        this.salsa = salsa;
+        this.cebolla = cebolla;
+        this.tipoSalsa = tipoSalsa;
+        this.sinGluten = sinGluten;
+        this.extraQueso = extraQueso;
+        this.piña = piña;
+        this.champiñones = champiñones;
+        this.jamon = jamon;
+    }
+
+    public String getMasa() {
+        return masa;
+    }
+
+    public void setMasa(String masa) {
+        this.masa = masa;
+    }
+
+    public boolean isRelleno() {
+        return relleno;
+    }
+
+    public void setRelleno(boolean relleno) {
+        this.relleno = relleno;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+
+    public boolean isSalsa() {
+        return salsa;
+    }
+
+    public void setSalsa(boolean salsa) {
+        this.salsa = salsa;
+    }
+
+    public boolean isCebolla() {
+        return cebolla;
+    }
+
+    public void setCebolla(boolean cebolla) {
+        this.cebolla = cebolla;
+    }
+
+    public String getTipoSalsa() {
+        return tipoSalsa;
+    }
+
+    public void setTipoSalsa(String tipoSalsa) {
+        this.tipoSalsa = tipoSalsa;
+    }
+
+    public boolean isSinGluten() {
+        return sinGluten;
+    }
+
+    public void setSinGluten(boolean sinGluten) {
+        this.sinGluten = sinGluten;
+    }
+
+    public boolean isExtraQueso() {
+        return extraQueso;
+    }
+
+    public void setExtraQueso(boolean extraQueso) {
+        this.extraQueso = extraQueso;
+    }
+
+    public boolean isPiña() {
+        return piña;
+    }
+
+    public void setPiña(boolean piña) {
+        this.piña = piña;
+    }
+
+    public boolean isChampiñones() {
+        return champiñones;
+    }
+
+    public void setChampiñones(boolean champiñones) {
+        this.champiñones = champiñones;
+    }
+
+    public boolean isJamon() {
+        return jamon;
+    }
+
+    public void setJamon(boolean jamon) {
+        this.jamon = jamon;
+    }
+
+    @Override
+    public String toString() {
+        return "Ingredientes de la pizza: " +
+                "masa " + masa + '\'' +
+                ", tiene relleno " + relleno +
+                ", es de tipo " + tipo +
+                ", tiene salsa " + salsa +
+                ", tipo de salsa '" + tipoSalsa + '\'' +
+                ", tiene cebolla " + cebolla +
+                ", es sin gluten " + sinGluten +
+                ", tiene extra de queso " + extraQueso +
+                ", tiene piña " + piña +
+                ", tiene champiñones " + champiñones +
+                ", tiene jamon " + jamon +
+                '}';
+    }
 }
