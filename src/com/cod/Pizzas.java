2e9c91c2af837844360c7a657bdf1815944f2391
Jose Alejandro Pereiro Gabian <jpereirogabian@danielcastelao.org>
2022-02-18 08:53:24 +0100

    Pizzas cocinadas

--- src/com/cod/Pizzas.java
+++ src/com/cod/Pizzas.java
@@ -30,108 +30,19 @@ public class Pizzas {
         this.jamon = jamon;
     }
 
-    public String getMasa() {
-        return masa;
-    }
-
-    public void setMasa(String masa) {
-        this.masa = masa;
-    }
-
-    public boolean isRelleno() {
-        return relleno;
-    }
-
-    public void setRelleno(boolean relleno) {
-        this.relleno = relleno;
-    }
-
-    public int getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(int tipo) {
-        this.tipo = tipo;
-    }
-
-    public boolean isSalsa() {
-        return salsa;
-    }
-
-    public void setSalsa(boolean salsa) {
-        this.salsa = salsa;
-    }
-
-    public boolean isCebolla() {
-        return cebolla;
-    }
-
-    public void setCebolla(boolean cebolla) {
-        this.cebolla = cebolla;
-    }
-
-    public String getTipoSalsa() {
-        return tipoSalsa;
-    }
-
-    public void setTipoSalsa(String tipoSalsa) {
-        this.tipoSalsa = tipoSalsa;
-    }
-
-    public boolean isSinGluten() {
-        return sinGluten;
-    }
-
-    public void setSinGluten(boolean sinGluten) {
-        this.sinGluten = sinGluten;
-    }
-
-    public boolean isExtraQueso() {
-        return extraQueso;
-    }
-
-    public void setExtraQueso(boolean extraQueso) {
-        this.extraQueso = extraQueso;
-    }
-
-    public boolean isPiña() {
-        return piña;
-    }
-
-    public void setPiña(boolean piña) {
-        this.piña = piña;
-    }
-
-    public boolean isChampiñones() {
-        return champiñones;
-    }
-
-    public void setChampiñones(boolean champiñones) {
-        this.champiñones = champiñones;
-    }
-
-    public boolean isJamon() {
-        return jamon;
-    }
-
-    public void setJamon(boolean jamon) {
-        this.jamon = jamon;
-    }
-
     @Override
     public String toString() {
-        return "Ingredientes de la pizza: " +
-                "masa " + masa + '\'' +
-                ", tiene relleno " + relleno +
-                ", es de tipo " + tipo +
-                ", tiene salsa " + salsa +
-                ", tipo de salsa '" + tipoSalsa + '\'' +
-                ", tiene cebolla " + cebolla +
-                ", es sin gluten " + sinGluten +
-                ", tiene extra de queso " + extraQueso +
-                ", tiene piña " + piña +
-                ", tiene champiñones " + champiñones +
-                ", tiene jamon " + jamon +
-                '}';
+        return "Pizzas" +
+                "masa='" + masa + '\'' +
+                ", relleno=" + relleno +
+                ", tipo=" + tipo +
+                ", salsa=" + salsa +
+                ", cebolla=" + cebolla +
+                ", tipoSalsa='" + tipoSalsa + '\'' +
+                ", sinGluten=" + sinGluten +
+                ", extraQueso=" + extraQueso +
+                ", piña=" + piña +
+                ", champiñones=" + champiñones +
+                ", jamon=" + jamon;
     }
 }
